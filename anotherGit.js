3119ea732ed5c43ef518ecada67dcc62404e8f6e
Sussertod <1037893409@qq.com>
2016-09-01 14:18:38 +0800

    add another js

---
+++ anotherGit.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Sussertod
+ * @Date:   2016-09-01 14:18:19
+ * @Last Modified by:   Sussertod
+ * @Last Modified time: 2016-09-01 14:18:24
+ */
+
+'use strict';
+
+console.log(window);
