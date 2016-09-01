dc1e62ae0922df7daa1a70b52da655ed3fc5b63d
Sussertod <1037893409@qq.com>
2016-09-01 14:21:55 +0800

    add branch0

---
+++ branch0.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Sussertod
+ * @Date:   2016-09-01 14:21:30
+ * @Last Modified by:   Sussertod
+ * @Last Modified time: 2016-09-01 14:21:40
+ */
+
+'use strict';
+
+console.log(body);
