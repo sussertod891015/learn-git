90ac58e37379f4e05985731aed72b5feb5fb78dd
Sussertod <1037893409@qq.com>
2016-09-01 14:06:26 +0800

    modified a script file

--- git.js
+++ git.js
@@ -1,10 +1,12 @@
 /*
  * @Author: sussertod
  * @Date:   2016-09-01 14:02:12
- * @Last Modified by:   sussertod
- * @Last Modified time: 2016-09-01 14:02:12
+ * @Last Modified by:   Sussertod
+ * @Last Modified time: 2016-09-01 14:06:04
  */
 
 'use strict';
 
-var a = 'hello!';
+var a = 'hello';
+
+var b = 'world!';
