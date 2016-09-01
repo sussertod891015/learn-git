db30e6273aaa67a0778e21f368814ae1bda8a81a
Sussertod <1037893409@qq.com>
2016-09-01 14:03:33 +0800

    just add a script file

---
+++ git.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: sussertod
+ * @Date:   2016-09-01 14:02:12
+ * @Last Modified by:   sussertod
+ * @Last Modified time: 2016-09-01 14:02:12
+ */
+
+'use strict';
+
+var a = 'hello!';
